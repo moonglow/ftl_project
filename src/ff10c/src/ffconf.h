3c511948831535287bde4a6a3e3d149c41a16b74
fxd <fxd>
2015-01-04 18:58:23 +0300

    apply patches for Chan FatFS fix f_unlink ( now clusters will be freed well ) and rtc fix

--- src/ff10c/src/ffconf.h
+++ src/ff10c/src/ffconf.h
@@ -200,9 +200,9 @@
 /---------------------------------------------------------------------------*/
 
 #define _FS_NORTC	1
-#define _NORTC_MON	0
-#define _NORTC_MDAY	0
-#define _NORTC_YEAR	0
+#define _NORTC_MON	1
+#define _NORTC_MDAY	1
+#define _NORTC_YEAR	2015
 /* The _FS_NORTC option switches timestamp feature. If the system does not have
 /  an RTC function or valid timestamp is not needed, set _FS_NORTC to 1 to disable
 /  the timestamp feature. All objects modified by FatFs will have a fixed timestamp
